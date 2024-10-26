49559f89d65d4e91a4ba40af5e67d53d392379a7
Risqi Romadhoni <38126197+risqiromadhoni@users.noreply.github.com>
2024-10-26 20:48:30 +0700

    ci(actions): init github actions (#1)
    * ci(actions): init github actions
    
    * ci(actions): add Node.js setup in pull-request.yml
    
    * ci(actions): update runs-on to ubuntu-latest in pull-request.yml
    
    * ci(actions): remove npm cache in pull-request.yml
    
    * ci(actions): update bun version to 1.1.33 and optimize dependency installation
    
    * chore: update package.json with new dependency

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,11 +1,11 @@
 export default {
   "./apps/web/*.{ts,tsx}": (files) => [
     "tsc -p ./apps/web/tsconfig.json --noEmit",
-    `biome check --apply ${files.join(" ")}`,
+    `biome check --write ${files.join(" ")}`,
   ],
   "./apps/api/*.ts": (files) => [
     "tsc -p ./apps/api/tsconfig.json --noEmit",
-    `biome check --apply ${files.join(" ")}`,
+    `biome check --write ${files.join(" ")}`,
   ],
-  "./**/*.{js,json,cs}": (files) => `biome check --apply ${files.join(" ")}`,
+  "./**/*.{js,css}": (files) => `biome check --write ${files.join(" ")}`,
 }
